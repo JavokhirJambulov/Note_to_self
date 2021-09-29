77293463095a47746ebe330a77bc1f63c45bdcc7
JavokhirJambulov <javohirjambulov@gmail.com>
2021-09-29 23:54:22 +0900

    Added SettingsActivity.kt

--- app/src/main/java/com/example/notetoself/MainActivity.kt
+++ app/src/main/java/com/example/notetoself/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.notetoself
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,6 +9,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
@@ -55,6 +58,27 @@ class MainActivity : AppCompatActivity() {
         val dialog = DialogShowNote()
         dialog.sendNoteSelected(noteList[noteToShow])
         dialog.show(supportFragmentManager, "")
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(this,
+                    SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
